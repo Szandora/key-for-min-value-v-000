3f54678a8d836ac445e51cee7bacb3d38c0518fc
Sheila Wittman <szandora789@gmail.com>
2018-06-09 17:00:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(weight_log)
+  min_key = nil
+  min_value = Float::INFINITY
   weight_log = {monday: 170, tuesday: 169, wednesday: 168, thursday: 167, friday: 166}
   weight_log.each do |day, weight|
-    weight(lowest)
-
+    if weight < min_value
+      min_value = weight
+      min_key = day
+    end
+  end
+  min_key
 end
