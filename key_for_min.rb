3b3aa1e8cd8e20889321f00aca7be7b11418a4b6
Sheila Wittman <szandora789@gmail.com>
2018-06-09 16:54:15 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(weight_log)
+  weight_log = {monday: 170, tuesday: 169, wednesday: 168, thursday: 167, friday: 166}
+  weight_log..min_by{|1,2| 1}
 
 end
